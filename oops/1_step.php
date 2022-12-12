94948c2f6dea488fee796b778be9a4b6bb19f8c6
Kamalesh Patel <kkp14397@gmail.com>
2022-12-13 00:13:46 +0530

    First Commit

---
+++ oops/1_step.php
@@ -0,0 +1,26 @@
+<?php
+class student{
+    public $name;
+    public $class;
+    function set_name($name){
+        $this->name = $name;
+    }
+    function set_class($class){
+        $this->class = $class;
+    }
+    function get_name(){
+        return $this->name;    
+    }
+    function get_class(){
+        return $this->class;
+
+    }
+
+}
+$data = new student();
+$data->set_name("Kamalesh");
+$data->set_class("B.tech");
+echo $data->get_name();
+echo $data->get_class();
+
+?>
